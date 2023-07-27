71dc4eea162a2902f297b840f224d7a4f89c39a0
domalee <136012609+domalee@users.noreply.github.com>
2023-07-26 17:00:32 -0700

    Update server.py

--- app1/server.py
+++ app1/server.py
@@ -1,5 +1,6 @@
 import sys
 sys.path.append('../')
+
 import random
 from libs.base import adder2
 from wsgiref.simple_server import make_server
@@ -8,16 +9,17 @@ from pyramid.response import Response
 import os
 
 def hello_world(request):
+    # Commit to trigger a deployment on a failed service
     x, y = random.randint(1, 100), random.randint(1, 100)
     res = adder2.add2(x, y)
     name = os.environ.get('NAME')
     if name == None or len(name) == 0:
         name = "world"
-    message = "Hello, " + name + "!\nThe sum is " + res
+    message = "Hello, " + name + "!\nThe sum of " + str(x) + " and " + str(y) + " is " + str(res)
     return Response(message)
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT"))
+    port = 8080
     with Configurator() as config:
         config.add_route('hello', '/')
         config.add_view(hello_world, route_name='hello')
